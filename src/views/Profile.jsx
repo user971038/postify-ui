d0db738714e2afc204436e5e5a713071c4b183e5
user971038 <A00226997@tec.mx>
2026-05-18 20:04:35 -0600

    commit

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -3,11 +3,12 @@ import { useParams } from 'react-router';
 
 const Profile = () => {
   const { userId } = useParams();
-  const url = import.meta.VITE_SERVER_POSTIFY;
-  console.log({ url });
+  const urlPosts = `http://localhost:8000/users/${userId}/posts`;
+  console.log({ urlPosts });
   
-  const { data, loading, error } = useFetch();
-  console.log('User ID:', userId);
+  const { data, loading, error } = useFetch(urlPosts);
+
+  console.log(data);
 
   return (
     <div className="flex flex-col">
@@ -18,7 +19,11 @@ const Profile = () => {
       </div>
 
       <div className="h-[560px] bg-pink-500 gap-4">
-        <div className="h-[150px] w-[120px] bg-purple-500"></div>
+        <div className="h-[150px] w-[120px] bg-purple-500">
+          {data.map((post) => (
+            <div key={post.id}>{post.title}</div>
+          ))}
+        </div>
       </div>
 
       <div className="h-[80px] bg-blue-500 gap-4">

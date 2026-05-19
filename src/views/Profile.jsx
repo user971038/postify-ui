ee760a085b46ae52fbbc73fe75fdb39ed571a2aa
user971038 <A00226997@tec.mx>
2026-05-18 19:31:43 -0600

    al día con la clase

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -4,10 +4,21 @@ import { useParams } from 'react-router';
 const Profile = () => {
   const { userId } = useParams();
   console.log('User ID:', userId);
-  
+
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="flex flex-col">
+      <div className="h-[80px] bg-green-500 gap-4">
+        <div>Yo</div>
+        <div>Followers</div>
+        <div>Posts</div>
+      </div>
+
+      <div className="h-[560px] bg-pink-500 gap-4">
+        <div className="h-[150px] w-[120px] bg-purple-500"></div>
+      </div>
+
+      <div className="h-[80px] bg-blue-500 gap-4">
+      </div>
     </div>
   );
 }
